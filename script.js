d439f1bd9ee7a9e7ba82e2dd47796f84bda2b02f
pokemonyay123 <baghet321123@gmail.com>
2024-04-09 12:24:22 -0300

    trial

--- script.js
+++ script.js
@@ -1,5 +1,59 @@
+var models = [
+    {
+        url: './assets/magnemite/scene.gltf',
+        scale: '0.5 0.5 0.5',
+        info: 'Magnemite, Lv. 5, HP 10/10',
+        rotation: '0 180 0',
+    },
+    {
+        url: './assets/articuno/scene.gltf',
+        scale: '0.2 0.2 0.2',
+        rotation: '0 180 0',
+        info: 'Articuno, Lv. 80, HP 100/100',
+    },
+    {
+        url: './assets/dragonite/scene.gltf',
+        scale: '0.08 0.08 0.08',
+        rotation: '0 180 0',
+        info: 'Dragonite, Lv. 99, HP 150/150',
+    },
+];
 
-// getting places from APIs
+var modelIndex = 0;
+
+var setModel = function (model, entity) {
+    if (model.scale) {
+        entity.setAttribute('scale', model.scale);
+    }
+
+    if (model.rotation) {
+        entity.setAttribute('rotation', model.rotation);
+    }
+
+    entity.setAttribute('gltf-model', model.url);
+
+    const div = document.querySelector('.instructions');
+    div.innerText = model.info;
+};
+
+function loadPokemonModels(places) {
+    let container = document.querySelector('#container');
+
+    places.forEach((place) => {
+        let latitude = place.location.lat;
+        let longitude = place.location.lng;
+
+        let model = document.createElement('a-entity');
+        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+
+        setModel(models[modelIndex], model);
+
+        model.setAttribute('animation-mixer', '');
+
+        container.appendChild(model);
+    });
+}
+
 function loadPlaces(position) {
     const params = {
         radius: 300,    // search places not farther than this value (in meters)
@@ -19,46 +73,26 @@ function loadPlaces(position) {
         &client_secret=${params.clientSecret}
         &limit=30 
         &v=${params.version}`;
+
     return fetch(endpoint)
-        .then((res) => {
-            return res.json()
-                .then((resp) => {
-                    return resp.response.venues;
-                })
-        })
+        .then((res) => res.json())
+        .then((data) => data.response.venues)
         .catch((err) => {
             console.error('Error with places API', err);
-        })
-};
+        });
+}
 
-
-window.onload = () => {
-    const scene = document.querySelector('a-scene');
-
-    // first get current user location
-    return navigator.geolocation.getCurrentPosition(function (position) {
-
-        // than use it to load from remote APIs some places nearby
-        loadPlaces(position.coords)
-            .then((places) => {
-                places.forEach((place) => {
-                    const latitude = place.location.lat;
-                    const longitude = place.location.lng;
-
-                    // add place name
-                    const placeText = document.createElement('a-link');
-                    placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-                    placeText.setAttribute('title', place.name);
-                    placeText.setAttribute('scale', '15 15 15');
-                    
-                    placeText.addEventListener('loaded', () => {
-                        window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
-                    });
-
-                    scene.appendChild(placeText);
-                });
-            })
-    },
+document.addEventListener('DOMContentLoaded', () => {
+    // First get current user location
+    navigator.geolocation.getCurrentPosition(
+        (position) => {
+            // Then use it to load from remote APIs some places nearby
+            loadPlaces(position.coords)
+                .then((places) => {
+                    loadPokemonModels(places);
+                })
+                .catch((err) => console.error('Error loading places', err));
+        },
         (err) => console.error('Error in retrieving position', err),
         {
             enableHighAccuracy: true,
@@ -66,4 +100,4 @@ window.onload = () => {
             timeout: 27000,
         }
     );
-};
+});
